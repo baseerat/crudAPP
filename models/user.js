629b9f201dac9c5f058e8aab5a5d900d8c41612c
baseerat <50256684+baseerat@users.noreply.github.com>
2020-11-26 13:02:06 +0530

    initial commit

---
+++ models/user.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, Sequelize) => {
+
+    const User = sequelize.define("user", {
+    
+    firstname: {
+    
+    type: Sequelize.STRING
+    
+    },
+    
+    lastname: {
+    
+    type: Sequelize.STRING
+    
+    },
+    email: {
+    
+        type: Sequelize.STRING
+        
+        },
+    password: {
+    
+            type: Sequelize.STRING
+            
+    }
+    
+    });
+    
+    
+    return User;
+    
+    };
